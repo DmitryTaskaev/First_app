e323b2e093e6dd4d911a1e6de4c6195e07ba152d
DmitryTaskaev <dimataskaev@bk.ru>
2022-06-17 11:52:25 +0500

    Chapter 2

--- app/src/main/java/com/bignerdranch/androin/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/androin/geomain/MainActivity.kt
@@ -3,13 +3,15 @@ package com.bignerdranch.androin.geomain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     public lateinit var trueButton : Button
     public lateinit var  falseButton : Button
-    public  lateinit var  nextButton: Button
+    public  lateinit var  nextButton: ImageButton
+    public  lateinit var  backButton: ImageButton
     public lateinit var questionTextView: TextView
     private  val questionBank = listOf(
         Question(R.string.quest_australia, true),
@@ -28,19 +30,25 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.button_true)
         falseButton = findViewById(R.id.button_false)
         nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
         questionTextView = findViewById(R.id.textView)
 
         trueButton.setOnClickListener {
-            Toast.makeText(this,R.string.True_Toast,Toast.LENGTH_SHORT).show()
-
-
+            CheckAnswer(true)
         }
         falseButton.setOnClickListener {
-            Toast.makeText(this,R.string.False_Toast,Toast.LENGTH_SHORT).show()
+            CheckAnswer(false)
         }
-
+        questionTextView.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size
+            UpdateQuestion()
+        }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            UpdateQuestion()
+        }
+        backButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
             UpdateQuestion()
         }
         UpdateQuestion()
@@ -49,5 +57,14 @@ class MainActivity : AppCompatActivity() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
-    private  fun CheckAnswer(UserAnswer: Boolean)
+    private  fun CheckAnswer(UserAnswer: Boolean) {
+        val correctAnwser = questionBank[currentIndex].answer
+        val messageResId = if (UserAnswer == correctAnwser) {
+            R.string.True_Toast
+        }
+        else {
+            R.string.False_Toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
 }
